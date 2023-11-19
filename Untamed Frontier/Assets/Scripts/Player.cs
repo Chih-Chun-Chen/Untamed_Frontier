668bfa271351dd28aab84087be3a9eafc72170c9
Chih-Chun-Chen <chen1226@go.byuh.edu>
2023-11-18 15:08:56 -1000

    Set up outline of game

---
+++ Untamed Frontier/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+public class Player : Person
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
