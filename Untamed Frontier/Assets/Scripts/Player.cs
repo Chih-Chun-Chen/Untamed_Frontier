931b06034a9f98ff031a519f0c623d302bad8d10
Chih-Chun-Chen <chen1226@go.byuh.edu>
2023-11-25 22:01:15 -1000

    Add player camera in front of  player head

--- Untamed Frontier/Assets/Scripts/Player.cs
+++ Untamed Frontier/Assets/Scripts/Player.cs
@@ -1,14 +1,36 @@
-public class Player : Person
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
 {
+    public GameObject playerCamera;
+    public GameObject playerHead;
+    public GameObject gun;
+
+    private Vector3 cameraPositionOffset;
+    private Vector3 cameraRotationOffset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        cameraPositionOffset = new Vector3(0, 0.03f, -0.39f);
+        if (playerCamera != null)
+        {
+            playerCamera.transform.localPosition = cameraPositionOffset;
+            playerCamera.transform.localRotation = Quaternion.Euler(cameraRotationOffset);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerCamera != null)
+        {
+            playerCamera.transform.position = playerHead.transform.position + cameraPositionOffset;
+        }
     }
+
+
 }
