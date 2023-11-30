f815dc9e5d7de77f92a1ac4793a557cc71d9c4c6
Chih-Chun-Chen <chen1226@go.byuh.edu>
2023-11-29 17:13:49 -1000

    Start to build the enemy function

--- Untamed Frontier/Assets/Scripts/Enemy.cs
+++ Untamed Frontier/Assets/Scripts/Enemy.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject playerHead;
+    public GameObject playerBody;
+    public GameObject gun;
+    public GameObject bulletPrefab;
+    public AudioClip bulletEffect;
+
+    private Rigidbody enemyRigidbody;
 
     void Start()
     {
